518dd75a82627250f4f32742d7409347f01c9713
Curran McConnell <curran@atidot.com>
2020-03-09 16:36:39 -0400

    Elaborated newtype example in terms of floats and doubles.

--- Haskell/SBV.hs
+++ Haskell/SBV.hs
@@ -1,21 +1,70 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/18.09.tar.gz --pure -i runhaskell -p z3 -p "pkgs.haskellPackages.ghcWithPackages (pkgs: with pkgs; [sbv])"
-{-# OPTIONS_GHC -Wall -Werror #-}
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/20.03-beta.tar.gz --pure -i runhaskell -p z3 -p "pkgs.haskellPackages.ghcWithPackages (pkgs: with pkgs; [sbv])"
+{-# OPTIONS_GHC -Wall -Werror           #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
-{-# LANGUAGE PackageImports #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE PackageImports             #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module SBV where
 
-import "sbv" Data.SBV
+import           "sbv" Data.SBV
+import qualified "sbv" Data.SBV.Internals as SI
+
+newtype LengthInIntegerMetres
+    = LengthInIntegerMetres Integer
+    deriving (Real, Integral, Num, Enum, Eq, Ord)
+
+type SLenIntMetres = SBV LengthInIntegerMetres
+
+instance HasKind LengthInIntegerMetres where
+    kindOf _ = KUnbounded
+
+instance SymVal LengthInIntegerMetres where
+    mkSymVal = SI.genMkSymVar KUnbounded
+    literal  = SI.genLiteral  KUnbounded
+    fromCV   = SI.genFromCV
+
+newtype LengthInFloatingMetres
+    = LengthInFloatingMetres Float
+    deriving (Real, Num, Enum, Eq, Ord, Fractional, Floating, RealFrac, RealFloat)
+
+type SLenFMetres = SBV LengthInFloatingMetres
+
+instance HasKind LengthInFloatingMetres where
+    kindOf _ = KFloat
+
+instance SymVal LengthInFloatingMetres where
+    mkSymVal = SI.genMkSymVar KFloat
+    literal (LengthInFloatingMetres l) = SI.SBV . SI.SVal KFloat . Left . SI.CV KFloat . SI.CFloat $ l
+    fromCV (SI.CV _ (SI.CFloat a)) = LengthInFloatingMetres a
+    fromCV c                 = error $ "SymVal.LengthInFloatingMetres: Unexpected non-float value: " ++ show c
+
+instance IEEEFloating LengthInFloatingMetres
+
+newtype LengthInDoubleMetres
+    = LengthInDoubleMetres Double
+    deriving (Real, Num, Enum, Eq, Ord, Fractional, Floating, RealFrac, RealFloat)
+
+type SLenDMetres = SBV LengthInDoubleMetres
+
+instance HasKind LengthInDoubleMetres where
+    kindOf _ = KDouble
+
+instance SymVal LengthInDoubleMetres where
+    mkSymVal = SI.genMkSymVar KDouble
+    literal (LengthInDoubleMetres l) = SI.SBV . SI.SVal KDouble . Left . SI.CV KDouble . SI.CDouble $ l
+    fromCV (SI.CV _ (SI.CDouble a)) = LengthInDoubleMetres a
+    fromCV c                 = error $ "SymVal.LengthInDoubleMetres: Unexpected non-float value: " ++ show c
+
+instance IEEEFloating LengthInDoubleMetres
 
 formula :: Symbolic SBool
 formula = do
-    x :: SInteger <- exists "x"
-    y :: SInteger <- exists "y"
-    constrain $ x^2 + y^2 .== 25
-    return $ 3 * x + 4 * y .== 0
+    n :: SLenIntMetres <- exists "n"
+    x :: SLenFMetres   <- exists "x"
+    y :: SLenDMetres   <- exists "y"
+    constrain $ y .< literal 3.14
+    return $ (n .== literal 3) .&& (sFromIntegral n .== x)
 
 main :: IO ()
-main = do
-   result <- sat formula
-   print result
+main = print =<< sat formula
